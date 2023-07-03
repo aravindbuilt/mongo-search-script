3783864e2851ed6e03b78dd1fe12bc34c287fa67
aravindbuilt <aravind.kumar@contentstack.com>
2023-07-03 17:03:38 +0530

    test

--- index.js
+++ index.js
@@ -2,54 +2,44 @@
 const { MongoClient } = require("mongodb");
 
 // Local Variables
-const clusterDbUrl = "";
-const clusterDbName = "";
-const clusterDbCollectionName = "";
-const clusterDbApiKeys = [""];
-const searchCollectionNames = ["", ""];
-const searchString = "";
+const dbUrl = "<coredb_mongo_uri>";
+const dbName = "rawcms_production";
+const apiKeys = [""];
+const searchCollectionNames = ["entries", "publish_objects"];
+const searchString = "stake.com";
 
 async function main() {
-  const clusterDbClient = new MongoClient(clusterDbUrl);
-  await clusterDbClient.connect();
-  const clusterDb = clusterDbClient.db(clusterDbName);
-  const clusterDbCollection = clusterDb.collection(clusterDbCollectionName);
+  const dbClient = new MongoClient(dbUrl);
+  await dbClient.connect();
+  const db = dbClient.db(dbName);
 
-  const clusterConnectionDocs = await clusterDbCollection
-    .find({ api_key: { $in: clusterDbApiKeys } })
-    .toArray();
-
-  for (const clusterConnectionDoc of clusterConnectionDocs) {
-    let stackConnectionUrl = clusterConnectionDoc.url;
-    let stackApiKey = clusterConnectionDoc.api_key;
-    let stackDbName = clusterConnectionDoc.dbName;
-
-    const stackDbClient = new MongoClient(stackConnectionUrl);
-    await stackDbClient.connect();
-    const stackDb = stackDbClient.db(stackDbName);
-    for (const searchCollectionName of searchCollectionNames) {
-      const searchCollection = stackDb.collection(
-        `${stackApiKey}.${searchCollectionName}`
+  for (let searchCollectionName of searchCollectionNames) {
+    for (let apiKey of apiKeys) {
+      let resultSetCount = await db
+        .collection(`${apiKey}.${searchCollectionName}`)
+        .count({});
+      console.error(
+        `Current Batch: ${apiKey}.${searchCollectionName}: ${resultSetCount}`
       );
-
       for (let iteration = 0; ; iteration = iteration + 100) {
-        const searchDocs = await searchCollection
+        let resultSet = await db
+          .collection(`${apiKey}.${searchCollectionName}`)
           .find({}, { limit: 100, skip: iteration })
           .toArray();
-        if (searchDocs.length == 0) break;
 
-        for (const searchDoc of searchDocs) {
-          const searchDocString = JSON.stringify(searchDoc);
-          if (searchDocString.includes(searchString)) {
+        if (resultSet.length == 0) break;
+
+        for (let doc of resultSet) {
+          let docString = JSON.stringify(doc);
+
+          if (docString.includes(searchString)) {
             console.log(
-              `${searchDoc.api_key},${searchDoc.uid},${searchDoc._id},${searchDoc.locale}`
+              `${apiKey}, ${apiKey}.${searchCollectionName}, ${doc._id}, ${doc.uid}`
             );
           }
         }
       }
     }
-
-    await stackDbClient.close();
   }
 
   await clusterDbClient.close();
@@ -57,8 +47,8 @@ async function main() {
 
 main()
   .then(() => {
-    console.log("Log: Success");
+    console.error("Log: Success");
   })
-  .then((e) => {
+  .catch((e) => {
     console.error("Log: Error: ", e);
   });
